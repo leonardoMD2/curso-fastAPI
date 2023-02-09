145deddbf72458cb09c8a1b00e2112b1885c8486
leonardo <martinezjleonardo@gmail.com>
2023-02-09 12:26:04 -0300

    Agregando comentarios al código de la creación de database, engine y session

--- config/database.py
+++ config/database.py
@@ -3,12 +3,17 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm.session import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
+#directorios de creación de la BD
 sqlite_file_name = '../database.sqlite'
+#obtenemos la dirección
 base_dir = os.path.dirname(os.path.realpath(__file__))
 
+#Unificamos el nombre con el directorio
 database_url = f'sqlite:///{os.path.join(base_dir,sqlite_file_name)}'
 
+#El primer paso es crear un "motor" de SQLAlchemy
 engine = create_engine(database_url, echo=True)
+
 Session = sessionmaker(bind=engine)
 
 Base = declarative_base()
